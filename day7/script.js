22a70561820553d23d24a49a463e46b46dd4066b
otavio <otavio.ramos@gmail.com>
2023-09-19 00:23:37 -0300

    pointer events

--- day7/script.js
+++ day7/script.js
@@ -1,4 +1,6 @@
 const search = document.querySelector('.searchicon');
 search.addEventListener('click', e => {
+    
     document.querySelector('#searchinput').classList.toggle('active');
+    //document.querySelector('.menu').classList.toggle('inactive');
 });
