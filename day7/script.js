5ca26fe0e7d9696e5543eb55d276f1556a1fd272
otavio <otavio.ramos@gmail.com>
2023-09-20 10:23:48 -0300

    day 7 final

--- day7/script.js
+++ day7/script.js
@@ -1,6 +1,14 @@
+const main = document.querySelector(".main");
+const sidemenu = document.querySelector(".sidemenu");
 const search = document.querySelector('.searchicon');
 search.addEventListener('click', e => {
     
     document.querySelector('#searchinput').classList.toggle('active');
     //document.querySelector('.menu').classList.toggle('inactive');
+});
+
+const menu = document.querySelector('.menu');
+menu.addEventListener('click', e => {
+    sidemenu.classList.toggle('side-open');
+    main.classList.toggle('main-close');
 });
